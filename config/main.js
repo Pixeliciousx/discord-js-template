eadc50956f564f5915771d3f989a11923808d9ac
Evelyn <120289114+Pixeliciousx@users.noreply.github.com>
2024-01-06 06:48:30 +0100

    Update main config file
    Added prefix and status support.

--- config/main.js
+++ config/main.js
@@ -1,5 +1,13 @@
 module.exports = {
   token: "",
   debug: false,
-  clientId: ""
+  clientId: "",
+  prefix: ".",
+  status: {
+    statusType: "idle", //online, idle, dnd, invisible
+    activityType: "Custom", //Playing, Streaming, Listening, Watching, Competing, Custom
+    name: "custom", //This will not be shown in custom statuses.
+    state: "Messing around in the console.", //Use for custom statuses. This is what is shown.
+    url: "" //For streaming only, pretty much.
+  }
 }
